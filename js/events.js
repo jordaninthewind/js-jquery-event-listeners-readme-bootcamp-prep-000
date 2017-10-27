0534e10e25e2bbe493c86ea9e4b7619f91fc4068
jordaninthewind <jordan.kline@gmail.com>
2017-10-27 23:11:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,3 +16,11 @@ function frameIt() {
 function submitIt() {
   $('form').on('submit', function(){alert("Your form is going to be submitted now.")});
 }
+
+function pressIt() {
+  $('input').on('keydown', function (key){
+    if (key.which === 71){
+      alert('You pressed G.');
+    } else {}
+  })
+}
